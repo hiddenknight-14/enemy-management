531d32423a042d71d5a9dbbf41dd0dbb878f1abc
hiddenknight-14 <hiddenknight14@gmail.com>
2025-10-12 19:05:28 +0530

    Update settings.py: updated db config

--- enemy_project/settings.py
+++ enemy_project/settings.py
@@ -76,16 +76,23 @@ WSGI_APPLICATION = 'enemy_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
-import os
+# Database configuration
+if os.getenv('RENDER'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
